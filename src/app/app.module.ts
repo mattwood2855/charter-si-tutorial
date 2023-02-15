b6deddd5faba49a95683bd054d5ccc8c9d4daa2b
Matthew Wood <c-matthew.wood@charter.com>
2023-02-15 11:53:24 -0800

    compilation fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,23 @@ import { LottieComponent } from './lottie/lottie.component';
 import { LottieModule } from 'ngx-lottie';
 import player from 'lottie-web';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AngularKiteCoreModule } from '@kite/angular-kite';
 
 export function playerFactory(): any {
   return player;
 }
 
 @NgModule({
-  declarations: [AppComponent, TutorialPageComponent, LottieComponent, ToolbarComponent],
+  declarations: [
+    AppComponent,
+    TutorialPageComponent,
+    LottieComponent,
+    ToolbarComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularKiteCoreModule,
     LottieModule.forRoot({ player: playerFactory }),
   ],
   providers: [],
